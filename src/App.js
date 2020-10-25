3b1e5dc20b2b99655d960c7832d54a08e25bdd90
田中 俊一朗 <shun.ichiro.tanaka16@gmail.com>
2020-10-26 01:44:34 +0900

    ホーム画面追加

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import Home from './pages/Home.js';
 import './sass/App.scss';
 
 function App() {
   return (
     <div className="App">
       <h1>Google Clone</h1>
+
+      {/* Home */}
+      <Home/>
+      {/* SearchPage */}
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>This is Home Page</h1>
+
+      <div className="home__header">
+        <div className="home__headerLeft">
+          {/* Link */}
+          {/* Link */}
+        </div>
+        <div className="home__headerRight">
+          {/* Link */}
+          {/* Link */}
+          {/* Icon */}
+          {/* Avatar */}
+        </div>
+      </div>
+
+      <div className="home__body">
+      </div>
+    </div>
+  )
+}
+
+export default Home
